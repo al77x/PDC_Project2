9dd63675662e3eeb9b98912de6e1357fdc050760
al77x <mss9953@autuni.ac.nz>
2023-06-02 18:24:04 +1200

    modified AskTheAudience class to change to GUI form

--- src/PDC_Project1/AskTheAudience.java
+++ src/PDC_Project1/AskTheAudience.java
@@ -4,7 +4,12 @@
  */
 package PDC_Project1;
 
+import java.util.ArrayList;
 import java.util.Random;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  *
@@ -17,18 +22,21 @@ public class AskTheAudience extends Lifeline
     {
         if(super.getUsed()) //check to see if the lifeline has been used, it can only be used once
         {
-            System.out.println("You've already asked the audience, you can't ask them anymore. Select a different Lifeline from the options or answer the question.");
-            return -1;
+            JOptionPane.showMessageDialog(null, "You've already asked the audience, you can't ask them anymore. Select a different Lifeline from the options or answer the question."); //shows a dialog box with the panel
+            return -1; 
         }
         
         //otherwise if it has not been used, the lifeline is being used in this scenario
-        System.out.println("You are getting help from the Audience!");
-        System.out.println("The audience are discussing their answers...");
-        System.out.println("\nAudience Results:");
+
+        //display to the user in the audience interaction dialog box
+        String text1 = "You are getting help from the Audience!";
+        String text2 = "The audience are discussing their answers..."; 
         
         int[] probability = new int[4]; //answers 1-4 (index 0-3)
         int correctAnswer = question.getCorrectAnswer() - 1;
         Random random = new Random();
+        
+        ArrayList<String> output = new ArrayList();
         
         //this much goes towards the correct answer
         probability[correctAnswer] = 40;
@@ -39,6 +47,7 @@ public class AskTheAudience extends Lifeline
         //calculating and displaying the audience's vote result for each answer. 
         for(int i = 0; i < 4; i++)
         {
+            String line = "";
             int generate = 0;
             
             if(remaining > 0)
@@ -49,17 +58,30 @@ public class AskTheAudience extends Lifeline
             probability[i] += generate;
             remaining -= generate;
             
-            System.out.println((i + 1) + ")");
+            line = line + (i+1) + ") ";
             
             for (int k = 0; k < probability[i]; k++)
             {
-                System.out.print("="); //prints the lines based on the probability of the answer
+                line = line + "="; //prints the lines based on the probability of the answer
             }
             
-            System.out.println("["+ probability[i] + "%" + "]");
+            line = line + probability[i] + "%";
+            
+            output.add(line);
         }
         
-        System.out.println("\nEnter your answer now: "); //after ask the audience is used
+        JPanel display = new JPanel();
+        display.setLayout(new BoxLayout(display, BoxLayout.Y_AXIS));
+        
+        display.add(new JLabel(text1));
+        display.add(new JLabel(text2));
+        
+        for (int i = 0; i < output.size(); i++)
+        {
+            display.add(new JLabel(output.get(i)));
+        }
+        
+        JOptionPane.showMessageDialog(null, display);
         
         super.setUsed(); //call method so that lifeline cannot be activated again
         
